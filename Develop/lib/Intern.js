60fac2ffb01085d9a8f5f4d59dd33afd9e9dd43e
Ashley Ikeh <ashley.ikeh25@gmail.com>
2020-12-27 07:37:47 -0500

    Set up the basic template for the intern class. Required the employee parent class at the top of the intern.js file. Extended the employee class and then module.exported the intern class.

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,10 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Intern = require("../lib/Employee");
+
+class Intern extends Employee {
+    constructor() {
+
+    }
+}
+
+module.exports = Intern;
