c210d460aa8dfb9c1e68bf7866467cd6e0b4b20a
Ashley Ikeh <ashley.ikeh25@gmail.com>
2020-12-27 06:32:56 -0500

    paused the engineer, intern, and manager test cases for now. Had to remove the employee class code because of a syntax error in the module.export.

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,21 +1,7 @@
 // TODO: Write code to define and export the Employee class
-class Employee{
-    constructor(name, id, email){
-        this.name = name;
-        this.id = id;
-        this.email = email;
-    }
+class Employee {
+    constructor() {
 
-    getId(){
-
-    }
-
-    getEmail(){
-
-    }
-
-    getRole() {
-        return Employee;
     }
 }
-module.Exports = Employee;
+module.exports = Employee;
