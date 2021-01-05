4d4db5dc3f1d32941569e7df09175d6b4c22f7e7
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-01-05 18:23:55 -0500

    Figured out how to return the role of each type of employee. Had to declare the role variable first and then return the employee type. For some reason I thought that if you did not declare a variable via the constructor parameters you could not use it. Glad I figured it out.

--- Develop/test/Engineer.test.js
+++ Develop/test/Engineer.test.js
@@ -6,7 +6,7 @@ xtest("Can set GitHub account via constructor", () => {
   expect(e.gitHub).toBe(testValue);
 });
 
-xtest("getRole() should return \"Engineer\"", () => {
+test("getRole() should return \"Engineer\"", () => {
   const testValue = "Engineer";
   const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
   expect(e.getRole()).toBe(testValue);
