ac2438de7b2298ac7088653f4ffd2b6d0fe7af2d
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-01-06 09:06:44 -0500

    Moved around the code to make it easier to read. Moved the commented out code to after the non commented code. Tried object destructuring to see if that would help solve the issue of the employee.Role not being a function but it did not. Still a WIP.

--- Develop/app.js
+++ Develop/app.js
@@ -1,3 +1,4 @@
+const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -9,6 +10,215 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+
+
+let employee = new Employee();
+let employees = [];
+
+const employeeQuestions = () => {
+inquirer.prompt([
+  {
+      type: "string",
+      name: "name",
+      message: "What is your name (first and last)?"
+  },
+
+  {
+      type: "number",
+      name: "id",
+      message: "What is your employee ID number?"
+  },
+
+  {
+      type: "string",
+      name: "email",
+      message: "What is your preferred email address?"
+  },
+
+  {
+      type: "list",
+      name: "role",
+      message: "What is your classification at this company?",
+      choices: ["Engineer", "Intern", "Manager"]
+  }
+])
+.then((answers) => {
+  if(answers.role === 'Engineer'){
+      inquirer.prompt([
+          {
+              type: "input",
+              name: "gitHub",
+              message: "What is your GitHub username?"
+          }
+      ]).then((completeEmployee) => {
+        let employee = {
+          ...answers,
+          ...completeEmployee,
+          };
+          employees.push(employee);
+          console.log(employees);
+          addMore();
+      });
+  }else if(answers.role === 'Intern'){
+      inquirer.prompt([
+          {
+              type: "input",
+              name: "school",
+              message: "What college or university do you attend?"
+          }
+      ]).then((completeEmployee) => {
+        let employee = {
+          ...answers,
+          ...completeEmployee,
+          };
+          employees.push(employee);
+          console.log(employees);
+          addMore();
+      });
+  } else if (answers.role === 'Manager'){
+      inquirer.prompt([
+          {
+              type: "input",
+              name: "officeNumber",
+              message: "What is your office number?"
+          }
+      ]).then((completeEmployee) => {
+        let employee = {
+          ...answers,
+          ...completeEmployee,
+          };
+        employees.push(employee);
+        console.log(employees);
+        addMore();
+      });
+  };
+
+});
+};
+
+
+
+const addMore = () => {
+    inquirer.prompt([
+        {
+            type: "confirm",
+            name: "addingMore",
+            message: "Do you want to add more employees?"
+        }
+    ]).then(({addingMore}) => {
+        if(addingMore) {
+            employeeQuestions();
+        } else {
+            console.log("Generating your custom employee webpage...");
+              render(employees);
+            };
+        });
+        
+    };
+
+
+
+employeeQuestions();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
+
+
+// const addMore = async () => {
+//     inquirer.prompt([
+//         {
+//             type: "confirm",
+//             name: "addingMore",
+//             message: "Do you want to add more employees?"
+//         }
+//     ]).then((response) => {
+//         if(response.addingMore === true) {
+//             employeeQuestions();
+//         } else {
+//             console.log("Generating your custom employee webpage...");
+//             async () => { 
+//               const response = await response.addingMore(false);
+//               render();
+//             };
+//         };
+        
+//     });
+
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // const employees = [];
 
@@ -296,154 +506,3 @@ const render = require("./lib/htmlRenderer");
 //     }
 
 // })
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-employee = new Employee();
-const employees = [];
-
-const employeeQuestions = () => {
-inquirer.prompt([
-  {
-      type: "string",
-      name: "name",
-      message: "What is your name (first and last)?"
-  },
-
-  {
-      type: "number",
-      name: "id",
-      message: "What is your employee ID number?"
-  },
-
-  {
-      type: "string",
-      name: "email",
-      message: "What is your preferred email address?"
-  },
-
-  {
-      type: "list",
-      name: "role",
-      message: "What is your classification at this company?",
-      choices: ["Engineer", "Intern", "Manager"]
-  }
-])
-.then((answers) => {
-  if(answers.role === 'Engineer'){
-      inquirer.prompt([
-          {
-              type: "input",
-              name: "gitHub",
-              message: "What is your GitHub username?"
-          }
-      ]).then((completeEmployee) => {
-        let employee = {
-          ...answers,
-          ...completeEmployee,
-          };
-          employees.push(employee);
-          console.log(employees);
-          addMore();
-      });
-  }else if(answers.role === 'Intern'){
-      inquirer.prompt([
-          {
-              type: "input",
-              name: "school",
-              message: "What college or university do you attend?"
-          }
-      ]).then((completeEmployee) => {
-        let employee = {
-          ...answers,
-          ...completeEmployee,
-          };
-          employees.push(employee);
-          console.log(employees);
-          addMore();
-      });
-  } else if (answers.role === 'Manager'){
-      inquirer.prompt([
-          {
-              type: "input",
-              name: "officeNumber",
-              message: "What is your office number?"
-          }
-      ]).then((completeEmployee) => {
-        let employee = {
-          ...answers,
-          ...completeEmployee,
-          };
-        employees.push(employee);
-        console.log(employees);
-        addMore();
-      });
-  };
-
-});
-};
-
-// const addMore = async () => {
-//     inquirer.prompt([
-//         {
-//             type: "confirm",
-//             name: "addingMore",
-//             message: "Do you want to add more employees?"
-//         }
-//     ]).then((response) => {
-//         if(response.addingMore === true) {
-//             employeeQuestions();
-//         } else {
-//             console.log("Generating your custom employee webpage...");
-//             async () => { 
-//               const response = await response.addingMore(false);
-//               render();
-//             };
-//         };
-        
-//     });
-
-// };
-
-const addMore = () => {
-    inquirer.prompt([
-        {
-            type: "confirm",
-            name: "addingMore",
-            message: "Do you want to add more employees?"
-        }
-    ]).then((response) => {
-        if(response.addingMore === true) {
-            employeeQuestions();
-        } else {
-            console.log("Generating your custom employee webpage...");
-              render();
-            };
-        });
-        
-    };
-
-
-
-employeeQuestions();
